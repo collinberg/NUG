6f74c2d04e1289db5ee2e74034b73e0a68813912
Collin <collin@SJI-4-Web.local>
2016-07-25 15:27:14 -0400

    commented basic functions

---
+++ app.js
@@ -0,0 +1,10 @@
+'user strick';
+var movies = array{};
+
+for (movie in movies){
+	var item;
+	//Create Item for Carousel
+	//Call API information for movie
+	//Add that informaiton into the carousel item (Post Name, year published)
+	//Add that item to the Carousel
+}
