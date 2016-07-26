24559392b20082a7ab6bec5d7975a81dfff7965b
Collin Berg <berg.collin@gmail.com>
2016-07-26 10:46:34 -0400

    set up server listen

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-'user strick';
-var movies = array{};
+'use strick';
 
-for (movie in movies){
-	var item;
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req,res){
+	res.send("<h1>Connection Made</h1>");
+	
+});
+
+app.listen(3000, function(){
+	console.log('The Frontend Server is Running por 3000');
+});
+
+//var movies = array();
+
+//for (movie in movies){
+//	var item;
 	//Create Item for Carousel
 	//Call API information for movie
 	//Add that informaiton into the carousel item (Post Name, year published)
 	//Add that item to the Carousel
-}
+//}
